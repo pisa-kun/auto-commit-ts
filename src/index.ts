aeef74bb609fabef84798adc9ce9bc8002797f72
pisa <pisa0401mst@gmail.com>
2022-11-24 02:30:47 +0900

    設定変更

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-{
-    const fs = require("fs");
+import * as path from 'path';
+import { promises as fs } from 'fs';
+import simpleGit from 'simple-git';
 
-    fs.appendFile('./test.txt', 'Hello!\n', (err: Error) => {
-        if (err) throw err;
-        console.log("test.txt に追記");
-    })
-}
+(async () => {
+    //console.log(`${__dirname}`);
+    const gitPath = path.resolve(__dirname, '..');
+    console.log(gitPath);
+
+    const git = simpleGit(gitPath);
+    const newFilePath = `${gitPath}/test.txt`;
+
+    try {
+        await fs.appendFile(newFilePath, 'Hello!\n', "utf-8");
+        
+        await git.add(newFilePath);
+        await git.commit("commit");
+        await git.push();
+    }
+    catch(err) {
+        console.log("Error", err);
+    }
+})();
